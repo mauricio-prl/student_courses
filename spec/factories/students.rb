53e292a52a16f4b34d5bf61ef4032b5d08b35fcc
Mauricio P <mauriciopdvg@gmail.com>
2019-09-13 22:30:30 -0300

    Start the tests

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :student do
-    
+    name { Faker::Name.name }
+    email { name.downcase.gsub(' ', '_') + '@email.com' }
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context 'create' do
+		let(:student) { create(:student) }
+
+		it { expect(student.name).to match(/(?<name>[A-Z]\w+)/) }
+		it { expect(student.email).to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/) }
+	end
 end
