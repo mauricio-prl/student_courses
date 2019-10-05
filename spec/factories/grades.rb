8e32d1d010128d9e0bbd22c60f6c0bc3d1b322c8
Mauricio P <mauriciopdvg@gmail.com>
2019-10-05 11:08:39 -0300

    delete some not used files

--- spec/factories/grades.rb
+++ spec/factories/grades.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :grade do
     name { Faker::Name.name }
     time { '5 months'}
+    course
   end
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Grade, type: :model do
 
 		it { expect(grade.name).to match(/(?<name>[A-Z]\w+)/) }
 		it { expect(grade.time).to match(/\d+ \w+/) }
+		it { expect(grade.course).to be_kind_of(Course)}
 	end
 end
