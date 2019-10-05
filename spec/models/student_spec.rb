51f61c207aa37cf28a32f9ebe4a9e63e6e9f1059
Mauricio P <mauriciopdvg@gmail.com>
2019-10-05 11:58:21 -0300

    write controller tests

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to have_many(:courses) }
 end
